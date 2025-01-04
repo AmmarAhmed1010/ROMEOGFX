07882d2a5ba4f648a3c87f72559bfc10462c4b03
Ammar Ahmed <ammar7298@gmail.com>
2025-01-04 17:49:32 +0500

    D

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import PricingPlans from "@/components/PricingPlans";
-import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
         <Hero />
         <FeaturedThumbnail />
         <Testimonials/>
-        {/* <Skills/> */}
         <Pricing/>
         <PricingPlans/>
         <Contact/>
