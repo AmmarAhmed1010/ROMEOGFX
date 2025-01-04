dd6746c6dc4d6e67ab154ddc43ec6d7bbf8982a6
Ammar Ahmed <ammar7298@gmail.com>
2025-01-05 00:18:57 +0500

    new aceritnity ui added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import FeaturedThumbnail from "@/components/FeaturedThumbnail";
 import Hero from "@/components/Hero";
+import Layout from "@/components/Layout";
 import Pricing from "@/components/Pricing";
 import PricingPlans from "@/components/PricingPlans";
+import ScrollImages from "@/components/ScrollImages";
 import Testimonials from "@/components/Testimonials";
 import WorkNavigation from "@/components/WorkItems";
 
@@ -10,6 +12,8 @@ export default function Home() {
     <>
       <div className="bg-[#040D16] px-4 md:px-16">
         <Hero />
+        <Layout/>
+        <ScrollImages/>
         <FeaturedThumbnail />
         <WorkNavigation/>
         <Testimonials/>
