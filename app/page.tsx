61e87130a1e569a662aa0eb20617e4347b737cf4
Ammar Ahmed <ammar7298@gmail.com>
2025-01-05 15:37:34 +0500

    GT

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import FeaturedThumbnail from "@/components/FeaturedThumbnail";
 import Hero from "@/components/Hero";
-import Layout from "@/components/Layout";
-import MenuNavbar from "@/components/MenuNavbar";
 import Navbar from "@/components/Navbar";
-import Pricing from "@/components/Pricing";
-import PricingPlans from "@/components/PricingPlans";
-import ScrollImages from "@/components/ScrollImages";
-import Testimonials from "@/components/Testimonials";
-import WorkNavigation from "@/components/WorkItems";
 
 export default function Home() {
   return (
