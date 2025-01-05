6e28bbc7028809ad65fe7cb654699f2fe13ff655
Ammar Ahmed <ammar7298@gmail.com>
2025-01-05 16:06:34 +0500

    FDA

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
       <div className="bg-[#040D16] px-4 md:px-16">
         {/* <MenuNavbar/> */}
-        <Navbar/>
+        {/* <Navbar/> */}
         <Hero />
     
         {/* <Layout/> */}
