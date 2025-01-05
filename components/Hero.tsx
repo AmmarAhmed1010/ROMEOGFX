ae3bd388f20cbbf46b7f870d0e2bddd5b7f215f7
Ammar Ahmed <ammar7298@gmail.com>
2025-01-05 15:55:27 +0500

    D

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,33 @@
+"use client"
 import React from 'react';
+import { Link } from 'react-scroll';
 
-const Hero: React.FC = () => {
+const Hero = () => {
   return (
     <div id="home" className="flex items-center justify-center min-h-screen text-yellow-500">
-      <div className="text-center italic space-y-6">
+      <div className="text-center space-y-6">
         {/* This block will display the text with two lines on mobile, and one line on desktop */}
-        <div className="text-5xl md:text-9xl font-semibold leading-tight">
+        <div className="text-5xl md:text-9xl  italic font-semibold leading-tight">
           <span className="block sm:inline">Creating for </span>
           <span className="block sm:inline">Creators</span>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex justify-center  space-x-4 md:pt-6 ">
+          {/* My Work Button */}
+          <Link to="myWork" smooth={true} duration={500} className="px-6 py-3 md:px-12 md:text-xl bg-[#29629E] text-white rounded-3xl hover:bg-red-600 cursor-pointer">
+            My Work
+          </Link>
+
+          {/* Contact Me Button */}
+          <a 
+            href="https://wa.me/+923158552233?text=Hello!%20I%20would%20like%20to%20get%20in%20touch."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-3 md:px-12 md:text-xl bg-[#29629E] text-white rounded-3xl  cursor-pointer"
+          >
+            Contact Me
+          </a>
         </div>
       </div>
     </div>
