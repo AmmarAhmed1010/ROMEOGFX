31ff52010473a8a89da86ff8d9eca0190559d59f
Ammar Ahmed <ammar7298@gmail.com>
2025-01-21 13:16:01 +0500

    D

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -4,7 +4,7 @@ import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-w
 export function BackgroundBeamsWithCollisionDemo() {
   return (
     <BackgroundBeamsWithCollision>
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center">
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] text-3xl">
             <span>RomeoGFX.</span>
