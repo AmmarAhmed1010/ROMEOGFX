6319d49d10eb6092b4e2f60e217235d9341dd7bf
Ammar Ahmed <ammar7298@gmail.com>
2025-01-21 13:36:58 +0500

    AD

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -3,7 +3,8 @@ import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-w
 
 export function BackgroundBeamsWithCollisionDemo() {
   return (
-    <BackgroundBeamsWithCollision>
+  <div id="home">
+      <BackgroundBeamsWithCollision>
       <div className="flex items-center justify-center">
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)] text-3xl">
@@ -15,5 +16,6 @@ export function BackgroundBeamsWithCollisionDemo() {
         </div>
       </div>
     </BackgroundBeamsWithCollision>
+  </div>
   );
 }
--- components/BackgroundGradientDemo.tsx
+++ components/BackgroundGradientDemo.tsx
@@ -3,6 +3,45 @@ import React from "react";
 import { BackgroundGradient } from "./ui/background-gradient";
 import Image from "next/image";
 
+const cardData = [
+  {
+    id: 1,
+    imageSrc: "/thumbnail/thumbnail (1).jpg",
+    altText: "jordans",
+    title: "Air Jordan 4 Retro Reimagined",
+    description:
+      "The Air Jordan 4 Retro Reimagined Bred will release on Saturday, February 17, 2024. Your best opportunity to get these right now is by entering raffles and waiting for the official releases.",
+    price: "$100",
+  },
+  {
+    id: 2,
+    imageSrc: "/thumbnail/thumbnail (2).jpg",
+    altText: "jordans",
+    title: "Air Jordan 4 Retro Reimagined",
+    description:
+      "The Air Jordan 4 Retro Reimagined Bred will release on Saturday, February 17, 2024. Your best opportunity to get these right now is by entering raffles and waiting for the official releases.",
+    price: "$100",
+  },
+  {
+    id: 3,
+    imageSrc: "/thumbnail/thumbnail (3).jpg",
+    altText: "jordans",
+    title: "Air Jordan 4 Retro Reimagined",
+    description:
+      "The Air Jordan 4 Retro Reimagined Bred will release on Saturday, February 17, 2024. Your best opportunity to get these right now is by entering raffles and waiting for the official releases.",
+    price: "$100",
+  },
+  {
+    id: 4,
+    imageSrc: "/thumbnail/thumbnail (4).jpg",
+    altText: "jordans",
+    title: "Air Jordan 4 Retro Reimagined",
+    description:
+      "The Air Jordan 4 Retro Reimagined Bred will release on Saturday, February 17, 2024. Your best opportunity to get these right now is by entering raffles and waiting for the official releases.",
+    price: "$100",
+  },
+];
+
 export function BackgroundGradientDemo() {
   return (
     <div id="plans" className="pb-10">
@@ -11,114 +50,32 @@ export function BackgroundGradientDemo() {
         My Plans
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {/* First Card */}
-        <BackgroundGradient className="rounded-[22px] p-4 bg-black">
-          <Image
-            src={`/thumbnail/thumbnail (1).jpg`} // Updated image source
-            alt="jordans"
-            height="400"
-            width="400"
-            className="object-contain rounded-3xl" // Added rounded class
-          />
-          <p className="text-base sm:text-xl text-white mt-4 mb-2">
-            Air Jordan 4 Retro Reimagined
-          </p>
-          <p className="text-sm text-white">
-            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-            February 17, 2024. Your best opportunity to get these right now is by
-            entering raffles and waiting for the official releases.
-          </p>
-         <div className="flex w-full justify-center items-center">
-         <button className="rounded-full border-2  border-purple-700 pl-4 pr-1 py-1 text-white flex  items-center space-x-1 bg-black mt-4 text-xs font-bold">
-            <span>Buy now </span>
-            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-              $100
-            </span>
-          </button>
-         </div>
-        </BackgroundGradient>
-
-        {/* Second Card */}
-        <BackgroundGradient className="rounded-[22px] p-4  bg-black">
-          <Image
-            src={`/thumbnail/thumbnail (2).jpg`} // Updated image source
-            alt="jordans"
-            height="400"
-            width="400"
-            className="object-contain rounded-3xl" // Added rounded class
-          />
-          <p className="text-base sm:text-xl text-white mt-4 mb-2">
-            Air Jordan 4 Retro Reimagined
-          </p>
-          <p className="text-sm text-white">
-            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-            February 17, 2024. Your best opportunity to get these right now is by
-            entering raffles and waiting for the official releases.
-          </p>
-          <div className="flex w-full justify-center items-center">
-         <button className="rounded-full border-2  border-purple-700 pl-4 pr-1 py-1 text-white flex  items-center space-x-1 bg-black mt-4 text-xs font-bold">
-            <span>Buy now </span>
-            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-              $100
-            </span>
-          </button>
-         </div>
-        </BackgroundGradient>
-
-        {/* Third Card */}
-        <BackgroundGradient className="rounded-[22px] p-4  bg-black">
-          <Image
-            src={`/thumbnail/thumbnail (3).jpg`} // Updated image source
-            alt="jordans"
-            height="400"
-            width="400"
-            className="object-contain rounded-3xl" // Added rounded class
-          />
-          <p className="text-base sm:text-xl text-white mt-4 mb-2">
-            Air Jordan 4 Retro Reimagined
-          </p>
-          <p className="text-sm text-white">
-            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-            February 17, 2024. Your best opportunity to get these right now is by
-            entering raffles and waiting for the official releases.
-          </p>
-          <div className="flex w-full justify-center items-center">
-         <button className="rounded-full border-2  border-purple-700 pl-4 pr-1 py-1 text-white flex  items-center space-x-1 bg-black mt-4 text-xs font-bold">
-            <span>Buy now </span>
-            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-              $100
-            </span>
-          </button>
-         </div>
-        </BackgroundGradient>
-
-        {/* Fourth Card */}
-        <BackgroundGradient className="rounded-[22px] p-4 bg-black">
-          <Image
-            src={`/thumbnail/thumbnail (4).jpg`} // Updated image source
-            alt="jordans"
-            height="400"
-            width="400"
-            className="object-contain rounded-3xl" // Added rounded class
-          />
-          <p className="text-base sm:text-xl text-white mt-4 mb-2">
-            Air Jordan 4 Retro Reimagined
-          </p>
-          <p className="text-sm text-white">
-            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-            February 17, 2024. Your best opportunity to get these right now is by
-            entering raffles and waiting for the official releases.
-          </p>
-          <div className="flex w-full justify-center items-center">
-         <button className="rounded-full border-2  border-purple-700 pl-4 pr-1 py-1 text-white flex  items-center space-x-1 bg-black mt-4 text-xs font-bold">
-            <span>Buy now </span>
-            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-              $100
-            </span>
-          </button>
-         </div>
-        </BackgroundGradient>
+      <div id="list" className="flex overflow-x-auto gap-6">
+        {cardData.map((card) => (
+          <BackgroundGradient
+          
+            key={card.id}
+            className="rounded-[22px] w-[300px] p-4 bg-black md:mx-auto"
+          >
+            <Image
+              src={card.imageSrc}
+              alt={card.altText}
+              height="400"
+              width="400"
+              className="object-contain rounded-3xl"
+            />
+            <p className="text-base sm:text-xl text-white mt-4 mb-2">{card.title}</p>
+            <p className="text-sm text-white">{card.description}</p>
+            <div className="flex w-full justify-center items-center">
+              <button className="rounded-full border-2 border-purple-700 pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold">
+                <span>Buy now </span>
+                <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+                  {card.price}
+                </span>
+              </button>
+            </div>
+          </BackgroundGradient>
+        ))}
       </div>
     </div>
   );
