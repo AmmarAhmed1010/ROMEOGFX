6056752930b5c4b743b43efc0a8f21ed479da255
Ammar Ahmed <ammar7298@gmail.com>
2025-01-23 12:33:29 +0500

    D

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { BackgroundBeamsWithCollisionDemo } from "@/components/BackgroundBeamsWithCollisionDemo";
 import { BackgroundGradientDemo } from "@/components/BackgroundGradientDemo";
 import Header from "@/components/Header";
+import ImagesMoving from "@/components/ImagesMoving";
+import Packages from "@/components/Packages";
 import { Pricing } from "@/components/Pricing";
+import Results from "@/components/Results";
 import Slider from "@/components/Slider";
 import WorkNavigation from "@/components/WorkItems";
 
@@ -13,9 +16,12 @@ export default function Home() {
         {/* <Hero /> */}
       <div className="px-4 md:px-16 ">
         <BackgroundBeamsWithCollisionDemo/>
+        <ImagesMoving/>
         <Slider/>
       {/* <ColourfulTextDemo/> */}
         <WorkNavigation />
+        <Packages/>
+        <Results/>
         <Pricing />
         <BackgroundGradientDemo />
       </div>
