c61a3888e1571124fb741c3f617bc3930305542b
Ammar Ahmed <ammar7298@gmail.com>
2025-01-27 12:01:19 +0500

    D

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import Slider from "@/components/Slider";
 import Achievements from "@/components/Achievements";
 import HeroSection from "@/components/HeroSection";
 import Plans from "@/components/Plans";
 import PriceList from "@/components/PriceList";
-import WorkCategoriesNew from "@/components/WorkCategoriesNew";
+// import WorkCategoriesNew from "@/components/WorkCategoriesNew";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="px-4 md:px-16 ">
         {/* hero section */}
         <HeroSection />
         {/* Featured */}
         <Slider />
         {/* Work categories */}
-        <WorkCategoriesNew/>
+        {/* <WorkCategoriesNew/> */}
         {/* Achievements */}
         <Achievements />
         {/* Price List */}
