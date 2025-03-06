78143407811882f42bb36adb3f50137b48d2a005
Ammar Ahmed <ammar7298@gmail.com>
2025-03-06 23:02:06 +0500

    changes

--- app/overlay/page.jsx
+++ app/overlay/page.jsx
@@ -1,18 +1,31 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    // Preloading videos for faster access
+    const videoList = Array.from({ length: 8 }).map((_, index) => {
+      const video = document.createElement("video");
+      video.src = `/overlay/overlay (${index + 1}).mp4`;
+      video.preload = "auto";
+      return video;
+    });
+    setVideos(videoList);
+  }, []);
+
   return (
     <div className="p-4">
-       <div className="bg-black text-white py-6 px-4">
-       <h1 className="text-3xl font-bold text-center mb-6">Overlays</h1>
-       </div>
+      <div className="bg-black text-white py-6 px-4">
+        <h1 className="text-3xl font-bold text-center mb-6">Overlays</h1>
+      </div>
       {/* Video Grid */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {Array.from({ length: 8 }).map((_, index) => (
+        {videos.map((video, index) => (
           <video
             key={index}
-            src={`/overlay/overlay (${index + 1}).mp4`} // Ensure correct file path
+            src={video.src}
             autoPlay
             muted
             loop
